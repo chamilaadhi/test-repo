767404ade38a5a04ca53d6ae6e4f26a9417956f2
chamilaadhi <cka.adhi@gmail.com>
2017-11-25 23:15:03 +0530

    source

---
+++ src/main/java/com/chamila/app/App.java
@@ -0,0 +1,13 @@
+package com.chamila.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
